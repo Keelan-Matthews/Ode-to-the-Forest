1dbfaff69e5dc403ef80fd6abc0f9e1d1ef87b17
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-03-09 20:06:15 +0200

    Added walk animations

--- Assets/Scripts/Runtime/PlayerMovement.cs
+++ Assets/Scripts/Runtime/PlayerMovement.cs
@@ -5,18 +5,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private int speed = 5;
+    
     private Vector2 _movement;
     private Rigidbody2D _rb;
-    [SerializeField] private int speed = 5;
+    private Animator _animator;
     
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     
     private void OnMovement(InputValue value)
     {
         _movement = value.Get<Vector2>();
+        
+        // Update the animator with the new movement values so it can play the correct animation
+        if (_movement.x != 0 || _movement.y != 0)
+        {
+            _animator.SetFloat("X", _movement.x);
+            _animator.SetFloat("Y", _movement.y);
+            
+            _animator.SetBool("IsWalking", true); //Tell the animator that the player is moving
+        } else
+        {
+            _animator.SetBool("IsWalking", false); //Tell the animator that the player is not moving
+        }
     }
     
     private void FixedUpdate()
