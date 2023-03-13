7967bf3a3ea65360a505e0e3c2d23fe74612c528
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-03-13 16:10:08 +0200

    Implemented TakeDamage from enemies

---
+++ Assets/Scripts/Runtime/Enemies/EnemyController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private int damage = 1;
+    
+    public float rotateSpeed = 0.025f;
+
+    public void MoveTowardsTarget(Vector2 targetPos)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+    
+    public void RotateTowardsTarget(Transform target)
+    {
+        var dir = target.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg +90f;
+        var rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, rotation, rotateSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
