3db1e7f9e7ddded937908086d4d332bd00558251
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-04-05 21:09:50 +0200

    Added red effect on knockback

--- Assets/Scripts/Runtime/KnockbackFeedback.cs
+++ Assets/Scripts/Runtime/KnockbackFeedback.cs
@@ -9,11 +9,20 @@ public class KnockbackFeedback : MonoBehaviour
     [SerializeField] private float strength = 10f, delay = 0.15f;
     
     public UnityEvent onBegin, onEnd;
+    private SpriteRenderer spriteRenderer;
+    
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void PlayFeedback(GameObject sender)
     {
         StopAllCoroutines();
         onBegin?.Invoke();
+        
+        // Get the sprite renderer of this object and change the color to hexadecimal
+        spriteRenderer.color = new Color(1f, 0.8537f, 0.8537f);
 
         // Get the direction of the sender and apply the force to the receiver
         var direction = (transform.position - sender.transform.position).normalized;
@@ -26,6 +35,7 @@ public class KnockbackFeedback : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         rb.velocity = Vector2.zero;
+        spriteRenderer.color = Color.white;
         onEnd?.Invoke();
     }
 }
