5d9e233c515f06c258e79bc906cf5be15bc858bd
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-07-03 12:17:00 +0200

    Implemented loading screen

--- Assets/Scripts/Runtime/ScenesManager.cs
+++ Assets/Scripts/Runtime/ScenesManager.cs
@@ -2,28 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour
 {
     // Make this a singleton
     public static ScenesManager Instance { get; private set; }
-    
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject menu;
+    [SerializeField] private Slider loadingBar;
+
     // Keep track of the current scene
     public static string currentSceneName = "Home";
 
     private void Awake()
     {
         Instance = this;
+        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Destroy duplicate GameAssets instances
+        }
     }
-    
+
     public static void LoadScene(string sceneName)
     {
-        // Stop the background music
-        // Load the scene with the given name
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        // Show the loading screen and hide the menu
+        Instance.loadingScreen.SetActive(true);
+        Instance.menu.SetActive(false);
+        
+        // Load the scene asynchronously
+        Instance.StartCoroutine(Instance.LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        var loadOperation = SceneManager.LoadSceneAsync(sceneName);
+
+        // While the scene is loading
+        while (!loadOperation.isDone)
+        {
+            var progress = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingBar.value = progress;
+
+            yield return null;
+        }
+
         // Set the current scene name to the given scene name
         currentSceneName = sceneName;
-        
+
         // PLay the background music for the scene
         switch (sceneName)
         {
@@ -33,6 +61,15 @@ public class ScenesManager : MonoBehaviour
             case "Forest":
                 AudioManager.PlayBackgroundMusic(AudioManager.Sound.ForestBackgroundMusic);
                 break;
+        }
+
+        // Hide the loading screen
+        loadingScreen.SetActive(false);
+
+        // If the scene is the home scene, show the menu
+        if (sceneName == "Home")
+        {
+            menu.SetActive(true);
         }
     }
 }
