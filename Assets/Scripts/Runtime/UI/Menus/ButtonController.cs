73cddee1baa3bbf5383e40ee449f42921fc56e56
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-07-26 09:34:16 +0200

    Disabled hover sound on button disable

--- Assets/Scripts/Runtime/UI/Menus/ButtonController.cs
+++ Assets/Scripts/Runtime/UI/Menus/ButtonController.cs
@@ -1,21 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
     public void OnButtonHover()
     {
+        if (!GetComponent<Button>().interactable) return;
         AudioManager.PlaySound(AudioManager.Sound.ButtonHover, transform.position);
     }
-    
+
     public void OnButtonClick()
     {
         AudioManager.PlaySound(AudioManager.Sound.ButtonClick, transform.position);
-    }
-    
-    public void OnDisabledButtonClick()
-    {
-        AudioManager.PlaySound(AudioManager.Sound.DisabledButtonClick, transform.position);
     }
 }
