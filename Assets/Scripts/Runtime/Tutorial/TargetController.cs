f907a5613d0da3bb0c60b14884eddf19fdc62ff8
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-07-26 19:35:27 +0200

    Animated targets

--- Assets/Scripts/Runtime/Tutorial/TargetController.cs
+++ Assets/Scripts/Runtime/Tutorial/TargetController.cs
@@ -6,15 +6,22 @@ public class TargetController : MonoBehaviour
 {
     private TargetManager _targetManager;
     public bool isHit;
-    
+    private Animator _targetAnimator;
+    private static readonly int Shot = Animator.StringToHash("Shot");
+
     private void Awake()
     {
         _targetManager = transform.parent.parent.GetComponent<TargetManager>();
+        
+        // Get the parent's animator
+        _targetAnimator = transform.parent.GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Bullet") || isHit) return;
         _targetManager.IncrementTargetsShot();
+        _targetAnimator.SetTrigger(Shot);
+        AudioManager.PlaySound(AudioManager.Sound.EnemyHit, transform.position);
         isHit = true;
     }
 }
