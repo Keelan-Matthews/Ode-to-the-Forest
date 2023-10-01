a29afb9855c86b98a390c414aa53f23d30f10839
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-10-01 14:19:46 +0200

    Added collector dialogue

---
+++ Assets/Scripts/Runtime/Objects/CollectorDialogueController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectorDialogueController : MonoBehaviour
+{
+    [SerializeField] private Dialogue collectorDialogue;
+
+    private GameObject dialogueComponent;
+    private DialogueController _dialogueController;
+    
+    private bool _isTalkingToCollector;
+
+    private void Awake()
+    {
+        dialogueComponent = RoomController.Instance.dialogueComponent;
+        _dialogueController = dialogueComponent.GetComponent<DialogueController>();
+    }
+
+    private void Update()
+    {
+        _isTalkingToCollector = GameManager.Instance.activeDialogue;
+    }
+
+    public void TalkToCollector()
+    {
+        if (_isTalkingToCollector) return;
+        _dialogueController.isPaused = false;
+        _dialogueController.SetDialogueAudio("Trader");
+        _dialogueController.isIntermittent = true;
+        _dialogueController.IsRandom = true;
+        _dialogueController.SetDialogue(collectorDialogue);
+        dialogueComponent.SetActive(true);
+
+        if (_dialogueController.isPaused)
+        {
+            _dialogueController.ResumeDialogue();
+        }
+        else
+        {
+            _dialogueController.StartDialogue();
+        }
+        
+        _isTalkingToCollector = true;
+    }
+    
+    public void StopTalkingToCollector()
+    {
+        _dialogueController.StopDialogue();
+        _isTalkingToCollector = false;
+    }
+}
