725615d0d3e922beab049acefbc153199107faf6
Keelan-Matthews <keelanmatthews123@gmail.com>
2023-10-22 11:55:37 +0200

    Added SFX

--- Assets/ScriptableObjects/Runtime/DungeonGeneration/Spawners/SpawnerData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Spawner.asset", menuName = "Spawners/Spawner", order = 0)]
-public class SpawnerData : ScriptableObject
-{
-    public GameObject itemToSpawn;
-    public float minSpawnRate, maxSpawnRate;
-}
